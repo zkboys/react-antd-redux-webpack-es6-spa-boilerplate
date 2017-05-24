d53af468e49d0c0054e2f2c4b69b8bf4ac771302
zkboys <876213774@qq.com>
2017-05-24 18:14:20 +0800

    文档 记录问题

--- src/page-init-state.js
+++ src/page-init-state.js
@@ -1,4 +1,42 @@
 export default {
+    billingPrivate: {
+    scope: 'billingPrivate',
+    toPrivateAccountName: '',
+    toPrivateSettleAccountNo: '',
+    toPrivateCnapsCode: '',
+},
+    billingPublic: {
+    scope: 'billingPublic',
+    toPublicAccountName: '',
+    toPublicSettleAccountNo: '',
+    toPublicCnapsCode: '',
+},
+    mearchSuccess: {
+    scope: 'mearchSuccess',
+    taskCode: '123',
+    registName: '123',
+    salesmanName: '123',
+    isAdd: '',
+    isModify: '',
+    isReturn: '',
+    isDraft: '',
+    userCode: '110665',
+    orgNo: '5138497260',
+},
+    actionsSetState: {
+    scope: 'actionsSetState',
+    sync: true,
+    a: {
+        b: {
+            c: ['ccc'],
+            c2: 'c2',
+        },
+        b1: [],
+        b2: 'b2',
+    },
+    d: 'd',
+    e: 'e',
+},
     promiseAjax: {
     scope: 'promiseAjax',
     sync: true,
