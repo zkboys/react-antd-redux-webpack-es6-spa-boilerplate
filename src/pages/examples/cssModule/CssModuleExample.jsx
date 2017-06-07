3b70c61a0e8b4087accac66d6fec6ea10fda3703
zkboys <876213774@qq.com>
2017-06-07 13:56:26 +0800

    完善注释

--- src/pages/examples/cssModule/CssModuleExample.jsx
+++ src/pages/examples/cssModule/CssModuleExample.jsx
@@ -21,6 +21,7 @@ export default class LayoutComponent extends Component {
           不层叠多个 class,只使用一个 class 把所有样式定义好;  所有样式通过 composes 组合来实现复用;
           不嵌套。
 
+         react-css-modules 优势
           我们不用再关注是否使用驼峰来命名 class 名;
           我们不用每一次使用 CSS Modules 的时候都关联 style 对象;
           使用 CSS Modules,容易使用 :global 去解决特殊情况,使用 react-css-modules 可写成 <div className="global-css" styleName="local-module"></div>,这种形式轻松对应全局和局
